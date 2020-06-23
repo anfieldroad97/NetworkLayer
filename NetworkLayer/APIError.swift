00362fbe0f246c59ba52a90ba907d1d343da5a44
Mirzhan <miko_555@icloud.com>
2020-06-23 17:12:22 +0600

    Add APIRequest and APIError

---
+++ NetworkLayer/APIError.swift
@@ -0,0 +1,14 @@
+//
+//  APIError.swift
+//  NetworkLayer
+//
+//  Created by Mirzhan Gumarov on 6/23/20.
+//  Copyright © 2020 Mirzhan Gumarov. All rights reserved.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case invalidURL
+    case requestFailed
+}
---
+++ NetworkLayer/APIRequest.swift
@@ -0,0 +1,28 @@
+//
+//  APIRequest.swift
+//  NetworkLayer
+//
+//  Created by Mirzhan Gumarov on 6/23/20.
+//  Copyright © 2020 Mirzhan Gumarov. All rights reserved.
+//
+
+import Foundation
+
+class APIRequest {
+    let method: HTTPMethod
+    let path: String
+    var queryItems: [URLQueryItem]?
+    var headers: [HTTPHeader]?
+    var body: Data?
+
+    init(method: HTTPMethod, path: String) {
+        self.method = method
+        self.path = path
+    }
+
+    init<Body: Encodable>(method: HTTPMethod, path: String, body: Body) throws {
+        self.method = method
+        self.path = path
+        self.body = try JSONEncoder().encode(body)
+    }
+}

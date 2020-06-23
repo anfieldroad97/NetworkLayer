7789f1576c45262aa690e7b99cb716035207f75d
Mirzhan <miko_555@icloud.com>
2020-06-23 17:08:58 +0600

    Add HTTPHeader

---
+++ NetworkLayer/HTTPHeader.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPHeader.swift
+//  NetworkLayer
+//
+//  Created by Mirzhan Gumarov on 6/23/20.
+//  Copyright © 2020 Mirzhan Gumarov. All rights reserved.
+//
+
+import Foundation
+
+struct HTTPHeader {
+    let field: String
+    let value: String
+}

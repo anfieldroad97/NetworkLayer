7e8a3b80ac478651bdf9eb24344dbfa2aee2f7aa
Mirzhan <miko_555@icloud.com>
2020-06-23 11:51:05 +0600

    init

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Mirzhan Gumarov on 6/23/20.
+//  Copyright © 2020 Mirzhan Gumarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
